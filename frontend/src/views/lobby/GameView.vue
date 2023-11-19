377e5a0c97d5c0e5d59c7e43f3f0163b21b8f0eb
Lucas Pires <piresnogueiralucas@gmail.com>
2023-11-19 20:23:26 -0300

    CHORE: setup p5

--- frontend/src/views/lobby/GameView.vue
+++ frontend/src/views/lobby/GameView.vue
@@ -1,18 +1,41 @@
 <script setup lang="ts">
-import { type Ref, ref, onMounted } from "vue";
+import { onMounted, onUnmounted } from "vue";
+import P5, { Image } from 'p5'
 
-const canvasElement: Ref<HTMLCanvasElement | undefined> = ref();
+
+let p5instance: P5
+
+onMounted(() => {
+  function sketch(p5: P5) {
+    p5.preload = function () {
+    }
+
+    p5.setup = function () {
+      p5.createCanvas(500, 500).parent("game")
+      p5.background("white")
+    }
+
+    p5.draw = function () {
+    }
+
+    p5.keyPressed = function () {
+    }
+
+    p5.keyReleased = function () {
+    }
+  }
+  p5instance = new P5(sketch)
+})
+
+onUnmounted(() => {
+  p5instance.remove()
+
+})
+
 </script>
 
 <template>
-  <main class="h-full w-full flex justify-center items-center">
-    <div class="w-4/5 h-4/5">
-      <canvas
-        ref="canvasElement"
-        class="border-2 border-neutral h-full w-full"
-        height="1000"
-        width="1000"
-      ></canvas>
-    </div>
-  </main>
+  <div id="game" class="h-full w-full flex justify-center items-center">
+
+  </div>
 </template>
