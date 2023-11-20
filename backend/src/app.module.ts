96d718ae900f41c4f253b2bef3d2b740f57f628b
Vinicius Oliveira <vinicius@myriad.codes>
2023-11-20 17:35:47 -0300

    feat: adds postgres database
    Co-authored-by: Marcelo Moreira <meritissimo1@users.noreply.github.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.NODE_ENV === "production",
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        database: configService.get("DB_NAME"),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        synchronize: configService.get("NODE_ENV") !== "production",
+      }),
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
