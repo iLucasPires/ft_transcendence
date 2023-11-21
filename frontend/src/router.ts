f474723c158593d3acad8322adfc18334ca590f8
Lucas Pires <piresnogueiralucas@gmail.com>
2023-11-21 19:54:22 -0300

    chore: refactoring the v-icon

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -6,41 +6,39 @@ import AboutView from "./views/AboutView.vue";
 import GameViewVue from "./views/lobby/GameView.vue";
 import ProfileViewVue from "./views/lobby/ProfileView.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: AboutView,
-    },
-    {
-      path: "/lobby",
-      name: "lobby",
-      component: LobbyView,
-      redirect: { name: "profile" },
-
-      children: [
-        {
-          path: "game",
-          name: "game",
-          component: GameViewVue,
-        },
-        {
-          path: "profile",
-          name: "profile",
-          component: ProfileViewVue,
-        },
-      ],
-    },
-  ],
-});
-
 export default function () {
-  return router;
+  return createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+      {
+        path: "/",
+        name: "home",
+        component: HomeView,
+      },
+      {
+        path: "/about",
+        name: "about",
+        component: AboutView,
+      },
+      {
+        path: "/lobby",
+        name: "lobby",
+        component: LobbyView,
+        redirect: { name: "profile" },
+
+        children: [
+          {
+            path: "game",
+            name: "game",
+            component: GameViewVue,
+          },
+          {
+            path: "profile",
+            name: "profile",
+            component: ProfileViewVue,
+          },
+        ],
+      },
+    ],
+  });
 }
