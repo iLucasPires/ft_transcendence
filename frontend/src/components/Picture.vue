f6f0488e78571b3dbf14d35f5cebba9c6c274599
Lucas Pires <piresnogueiralucas@gmail.com>
2023-11-21 19:52:57 -0300

    chore: separating some components

---
+++ frontend/src/components/Picture.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps({
+  size: { type: String, default: "md" },
+  url: { type: String, required: true },
+  alt: { type: String, required: true },
+  maskType: { type: String, default: "circle" },
+});
+
+const size = computed(() => {
+  switch (props.size) {
+    case "sm":
+      return "w-10 h-10";
+    case "md":
+      return "w-20 h-20";
+    case "lg":
+      return "w-40 h-40";
+    default:
+      return "w-full h-full";
+  }
+});
+</script>
+
+<template>
+  <div :class="` ${size}`">
+    <img
+      :src="url"
+      :alt="alt"
+      :class="`object-scale-down mask mask-${maskType}`"
+    />
+  </div>
+</template>
---
+++ frontend/src/components/Typography.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps({
+  level: { type: Number, default: 0 },
+  italic: { type: Boolean, default: false },
+  weight: { type: String, default: "normal" },
+  size: { type: String, default: "md" },
+  color: { type: String, default: "neutral" },
+  align: { type: String, default: "left" },
+  transform: { type: String, default: "none" },
+  type: { type: String, default: "normal" },
+});
+
+const headings = computed(() => {
+  if (props.level == 0) return "p";
+  else return "h" + props.level;
+});
+</script>
+
+<template>
+  <component
+    :is="headings"
+    :class="`text-${color} text-${size} font-${weight} text-${align} text-${transform} font-${type}`"
+  >
+    <slot></slot>
+  </component>
+</template>
