3cf5a031195229d4a204d161f2e96fdb3147ccb0
Vinicius Oliveira <vinicius@myriad.codes>
2023-11-21 19:31:21 -0300

    feat: implements logout

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  InternalServerErrorException,
+  Post,
+  Req,
+  UseGuards,
+} from "@nestjs/common";
 import { FortyTwoAuthGuard } from "./guards/forty-two.guard";
 import { Request } from "express";
+import { IsAuthenticatedGuard } from "./guards/authenticated.guard";
 
 @Controller("auth")
 export class AuthController {
@@ -12,5 +22,18 @@ export class AuthController {
   @UseGuards(FortyTwoAuthGuard)
   fortyTwoCallback(@Req() req: Request) {
     return req.user;
+  }
+
+  @Post("logout")
+  @UseGuards(IsAuthenticatedGuard)
+  async logout(@Req() request: Request) {
+    const logoutError = await new Promise((resolve) =>
+      request.logOut({ keepSessionInfo: false }, (error) => resolve(error)),
+    );
+
+    if (logoutError) {
+      throw new InternalServerErrorException("Could not logout user");
+    }
+    return { logout: true };
   }
 }
