fdd22cf16cd892469e758c4256d7ae85f7a4af1e
Lucas Pires <piresnogueiralucas@gmail.com>
2023-11-21 22:54:01 -0300

    feat: add page 404

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold mb-5">404</h1>
+        <img
+          src="https://media.giphy.com/media/HL4zwTG6PV9hm/giphy.gif"
+          alt="404"
+        />
+        <p class="py-6">Page not found. Check the address or 
+            <RouterLink to="/" class="text-primary">go back</RouterLink>
+        </p>
+        
+      </div>
+    </div>
+  </div>
+</template>
