2cb31f583d44d3cc7245919696de751b3952f18e
Vinicius Oliveira <vinicius@myriad.codes>
2023-11-22 11:04:39 -0300

    chore: adds return type to FortyTwoAuthGuard

--- backend/src/auth/guards/forty-two.guard.ts
+++ backend/src/auth/guards/forty-two.guard.ts
@@ -3,7 +3,7 @@ import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
 export class FortyTwoAuthGuard extends AuthGuard("forty-two") {
-  async canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     await super.canActivate(context);
 
     const request = context.switchToHttp().getRequest();
