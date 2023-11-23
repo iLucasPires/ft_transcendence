d7d7858291cca7a4352e3396cefa6b44947f41c8
Lucas Pires <piresnogueiralucas@gmail.com>
2023-11-23 12:03:02 -0300

    chore: transform some components into global

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,8 +6,13 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { OhVueIcon } from "oh-vue-icons";
 
+import Typography from "./components/Typography.vue";
+import Picture from "./components/Picture.vue";
+
 createApp(App)
+  .component("Typography", Typography)
+  .component("Picture", Picture)
+  .component("Icon", OhVueIcon)
   .use(createPinia())
   .use(createRouter())
-  .component("Icon", OhVueIcon)
   .mount("#app");
