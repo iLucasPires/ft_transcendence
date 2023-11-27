20a3f9367b776976332fa207000fbe99eca374a4
Vinicius Oliveira <vinicius@myriad.codes>
2023-11-27 16:52:54 -0300

    feat: adds /api/me for session interfacing

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Redirect } from "@nestjs/common";
+import { Controller, Get, Redirect, Req, UseGuards } from "@nestjs/common";
+import { IsAuthenticatedGuard } from "./auth/guards/authenticated.guard";
+import { Request } from "express";
 
 @Controller()
 export class AppController {
@@ -9,5 +11,11 @@ export class AppController {
   @Get("/health")
   getHealth() {
     return "OK";
+  }
+
+  @Get("/me")
+  @UseGuards(IsAuthenticatedGuard)
+  getMe(@Req() req: Request) {
+    return req.user;
   }
 }
