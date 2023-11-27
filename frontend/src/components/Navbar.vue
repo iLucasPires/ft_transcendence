64b97463031c8ac05e576c47215b4f0b71e71122
Lucas Pires <piresnogueiralucas@gmail.com>
2023-11-27 16:34:58 -0300

    feat: add dialogue when exiting the game

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,29 +1,66 @@
 <script setup lang="ts">
 import { ref, type Ref } from "vue";
-import ItemNavBar from "./ItemNavBar.vue";
-import useStore from "@/store";
 
-const store = useStore();
+import { router } from "@/router";
+import UseData from "@/store";
+import ItemNavBar from "@/components/ItemNavBar.vue";
 
-const isMenuOpen: Ref<boolean> = ref(true);
+const store = UseData();
+const menuOpen: Ref<boolean> = ref(true);
+const modal = ref<HTMLDialogElement | null>(null);
+
+function handleclick(url: string) {
+  if (store.status.isGame) {
+    modal.value?.showModal();
+  } else {
+    router.push(url);
+  }
+}
 </script>
 
 <template>
+  <dialog ref="modal" class="modal">
+    <div class="modal-box">
+      <h3 class="font-bold text-lg">Warning</h3>
+      <p class="py-4">
+        You are currently in a game, if you leave you will lose the game.
+      </p>
+      <div class="modal-action">
+        <form method="dialog">
+          <div class="join">
+            <button @click="modal?.close()" class="btn join-item">
+              Back to game
+            </button>
+            <button
+              @click="
+                store.status.isGame = false;
+                store.gameData?.remove();
+                modal?.close();
+              "
+              class="btn join-item btn-primary"
+            >
+              get out
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </dialog>
   <aside
-    :class="{ 'w-64': isMenuOpen, 'w-24': !isMenuOpen }"
+    :class="{ 'w-64': menuOpen, 'w-24': !menuOpen }"
     class="p-5 flex flex-col bg-base-300 transition-all duration-300"
   >
     <div class="container-center-row gap-2 w-full">
       <Icon
         scale="1.5"
         name="md-keyboarddoublearrowleft"
-        :class="{ 'transform -rotate-180': !isMenuOpen }"
-        @click="isMenuOpen = !isMenuOpen"
+        :class="{ 'transform -rotate-180': !menuOpen }"
+        @click="menuOpen = !menuOpen"
       />
     </div>
     <nav class="flex flex-col flex-1 justify-between mt-10">
       <ul>
-        <RouterLink
+        <ItemNavBar
           v-for="item in [
             { url: '/profile', icon: 'md-person', text: 'Profile' },
             { url: '/game', icon: 'io-game-controller', text: 'Game' },
@@ -38,21 +75,18 @@ const isMenuOpen: Ref<boolean> = ref(true);
               text: 'History',
             },
           ]"
+          @click="handleclick(item.url)"
           :to="item.url"
           :key="item.url"
+          :menuOpen="menuOpen"
+          :icon="item.icon"
         >
-          <ItemNavBar
-            :isMenuOpen="isMenuOpen"
-            :url="item.url"
-            :icon="item.icon"
-          >
-            {{ item.text }}
-          </ItemNavBar>
-        </RouterLink>
+          {{ item.text }}
+        </ItemNavBar>
       </ul>
     </nav>
     <button class="w-full dropdown dropdown-right dropdown-end">
-      <ItemNavBar type="span" :isMenuOpen="isMenuOpen" icon="md-settings">
+      <ItemNavBar type="span" :menuOpen="menuOpen" icon="md-settings">
         Settings
       </ItemNavBar>
       <ul
