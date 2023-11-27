2ea49e4853aa52a15523328823332e92323e53e4
Lucas Pires <piresnogueiralucas@gmail.com>
2023-11-27 16:16:51 -0300

    refactor: export router correctly

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,21 @@
 import App from "./App.vue";
-import createRouter from "./router";
+import { router } from "./router";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { OhVueIcon } from "oh-vue-icons";
 
-import "@/design/icons"
+import "@/design/icons";
 import Typography from "./components/Typography.vue";
 import Picture from "./components/Picture.vue";
+
+const pinia = createPinia();
 
 createApp(App)
   .provide("backendUrl", import.meta.env.VITE_BACKEND_URL)
   .component("Typography", Typography)
   .component("Picture", Picture)
   .component("Icon", OhVueIcon)
-  .use(createPinia())
-  .use(createRouter())
+  .use(pinia)
+  .use(router)
   .mount("#app");
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import LoginView from "./views/LoginView.vue";
 import LobbyView from "./views/LobbyView.vue";
 import AboutView from "./views/AboutView.vue";
-
 import GameViewVue from "./views/GameView.vue";
 import ProfileViewVue from "./views/ProfileView.vue";
-
+import NotFoundView from "./views/NotFoundView.vue";
 
 const routes = [
   {
     path: "/:catchAll(.*)",
     name: "not-found",
-    component: () => import("./views/NotFoundView.vue"),
+    component: NotFoundView,
   },
   {
     path: "/login",
@@ -41,22 +41,22 @@ const routes = [
       },
     ],
   },
-]
+];
 
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
 
-export default function () {
-  const router = createRouter({history: createWebHistory(import.meta.env.BASE_URL), routes });
+router.beforeEach((to, from, next) => {
+  // We check if a flag cookie is set, if not we redirect to the login page
+  const isCookieSet = document.cookie.indexOf("connect.flag=") !== -1;
 
-  router.beforeEach((to, from, next) => {
-    // We check if a flag cookie is set, if not we redirect to the login page
-    const isCookieSet = document.cookie.indexOf("connect.flag=") !== -1;
+  if (!isCookieSet && to.name !== "login") {
+    return next({ name: "login" });
+  }
 
-    if (!isCookieSet && to.name !== "login") {
-      return next({ name: "login" });
-    } 
+  next();
+});
 
-    next();
-  });
-
-  return (router);
-}  
+export { router, routes };
