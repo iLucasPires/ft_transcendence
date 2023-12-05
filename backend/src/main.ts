117655996e202e4abeaf58cc5350ad99b60a5eb7
Vinicius Oliveira <vinicius@myriad.codes>
2023-12-05 16:08:40 -0300

    fix: enables shutdown hooks

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("/api/docs", app, document);
 
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
