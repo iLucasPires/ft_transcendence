dcf18e4cab7007a0559d1f28f451ab29438c22fd
Vinicius Oliveira <vinicius@myriad.codes>
2023-12-05 23:01:04 -0300

    chore: adds request metadata to controllers

--- backend/src/users/dto/find-or-create-user.dto.ts
+++ backend/src/users/dto/find-or-create-user.dto.ts
@@ -1,13 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNumberString, IsString, Length } from "class-validator";
 
 export class FindOrCreateUserDto {
+  @ApiProperty({
+    description: "The user's intra ID",
+    example: 10,
+  })
   @IsNumberString()
   intraId: number;
 
+  @ApiProperty({
+    description: "The user's username",
+    example: "dezano",
+  })
   @IsString()
   @Length(1, 12)
   username: string;
 
+  @ApiProperty({
+    description: "The user's email",
+    example: "dezano@42sp.org.br",
+  })
   @IsEmail()
   email: string;
 }
--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, Length } from "class-validator";
 
 export class UpdateUserDto {
+  @ApiProperty({
+    description: "The user's new username",
+    example: "dezano",
+  })
   @IsString()
   @Length(1, 12)
   username: string;
