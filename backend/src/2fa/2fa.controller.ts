d4c4520a045ec038c72a16523f8798d2e8130f9e
Vinícius Gonçalves de Oliveira <vinicius@myriad.codes>
2023-12-19 19:05:22 +0000

    chore: adds swagger metadata to 2fa generate

--- backend/src/2fa/2fa.controller.ts
+++ backend/src/2fa/2fa.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Post, Req, Res, UseGuards } from "@nestjs/common";
+import {
+  Controller,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from "@nestjs/common";
 import { TwoFactorAuthService } from "./2fa.service";
 import { IsAuthenticatedGuard } from "../auth/guards/authenticated.guard";
 import { UserRequest } from "../users/interfaces";
 import { Response } from "express";
+import { ApiProduces, ApiResponse } from "@nestjs/swagger";
 
 @Controller("/auth/2fa")
 export class TwoFactorAuthController {
@@ -10,6 +18,11 @@ export class TwoFactorAuthController {
 
   @Post("generate")
   @UseGuards(IsAuthenticatedGuard)
+  @ApiProduces("image/png")
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: "Generated new 2FA secret for user.",
+  })
   async generate2FASecret(@Req() req: UserRequest, @Res() res: Response) {
     const user = req.user;
     const otpAuthUrl = await this.twoFactorAuthService.generate2faSecret(user);
