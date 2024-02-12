171aa135c8b47ee52c0d31b5781ca768c8788038
Lucas Pires <piresnogueiralucas@gmail.com>
2024-02-12 19:50:33 +0000

    Add MCardChat component and refactor ChatView.vue

---
+++ apps/frontend/src/components/molecules/MCardChat.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { iMessage } from "@/types/props";
+
+defineProps<{
+    avatarUrl: string;
+    author : string;
+    content: string;
+}>();
+</script>
+
+<template>
+  <li class="border-card p-4">
+    <div class="max-w-5xl flex items-center gap-2">
+      <div class="avatar">
+        <div class="w-16 rounded-full bg-base-200">
+            <img
+                :src="avatarUrl || `https://robohash.org/${author}.png`"
+                :alt="`avatar of ${author}`"
+            />
+        </div>
+      </div>
+      <div class="w-full">
+        <h2 class="text-md font-bold" v-text="author" />
+        <p class="text-md text-pretty break-words whitespace-pre-wrap" v-text="content" />
+      </div>
+    </div>
+  </li>
+</template>
--- apps/frontend/src/pages/Lobby/ChatView.vue
+++ apps/frontend/src/pages/Lobby/ChatView.vue
@@ -76,21 +76,15 @@ const handleClickChat = (chat: iChannel) => {
 
       <div class="w-full border-card separate flex flex-col h-full">
         <div class="h-full flex flex-col gap-2">
-          <div v-if="currentChatMessages.length !== 0" class="overflow-y-auto h-full flex flex-col-reverse py-2">
+          <div v-if="currentChatMessages.length" class="overflow-y-auto h-full flex flex-col-reverse py-2">
             <ul class="flex flex-col gap-2">
-              <li class="border-card p-4" v-for="message in currentChatMessages" :key="message.id">
-                <div class="max-w-5xl flex items-center gap-2">
-                  <div class="avatar">
-                    <div class="w-16 rounded-full bg-base-200">
-                      <img :src="message.author.avatarUrl || `https://robohash.org/${message.author.username}.png`" :alt="`avatar of ${message.author.username}`" />
-                    </div>
-                  </div>
-                  <div class="w-full">
-                    <h2 class="text-md font-bold" v-text="message.author.username" />
-                    <p class="text-md text-pretty break-words whitespace-pre-wrap" v-text="message.content" />
-                  </div>
-                </div>
-              </li>
+              <MCardChat
+                v-for="msg in currentChatMessages"
+                :key="msg.id"
+                :avatarUrl="msg.author.avatarUrl"
+                :author="msg.author.username"
+                :content="msg.content"
+              />
             </ul>
           </div>
           <div v-else class="flex items-center justify-center h-full">
