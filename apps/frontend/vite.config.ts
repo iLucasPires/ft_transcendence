8e76350e264152098daf91b84a14afccd2038f17
Lucas Pires <piresnogueiralucas@gmail.com>
2024-02-15 21:26:52 +0000

    chore: add auto import
    Co-authored-by: Vinícius Gonçalves de Oliveira <vinicius@myriad.codes>
    Co-authored-by: Marcelo Moreira <meritissimo1@users.noreply.github.com>

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,17 +4,31 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import RadixVueResolver from "radix-vue/resolver";
 
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      dts: true,
+      imports: [
+        "vue",
+        "pinia",
+        "vue-router",
+        { "@/stores/appStore": ["useAppStore"] },
+        { "@/stores/meStore": ["useMeStore"] },
+        { "@/stores/chatStore": ["useChatStore"] },
+      ],
+    }),
     Components({
       dts: true,
+      resolvers: [RadixVueResolver()],
     }),
   ],
   resolve: {
-    alias: { 
+    alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
