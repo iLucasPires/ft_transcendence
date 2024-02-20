dac8ad69c55a24f70d6d27a87f904023613286dc
Vinicius Oliveira <vinicius@myriad.codes>
2024-02-20 16:46:24 -0300

    fix: use refs

--- apps/frontend/src/components/organisms/ODetailChat.vue
+++ apps/frontend/src/components/organisms/ODetailChat.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-const chatStore = useChatStore();
 import { chatSocket } from "@/socket";
+
+const chatStore = useChatStore();
+const { currentChat, currentChatPhoto, currentChatName, currentChatMembers } = storeToRefs(chatStore);
 
 const handleLeaveChat = () => {
   chatSocket.emit("leaveChannel", chatStore.currentChatId, () => {
@@ -10,12 +12,13 @@ const handleLeaveChat = () => {
 </script>
 
 <template>
-  <div v-if="chatStore.currentChat" class="flex overflow-hidden flex-col items-center border-card full p-4 space-y-2">
-    <h1 class="text-2xl font-bold">{{ chatStore.currentChatName }}</h1>
+  <div v-if="currentChat" class="flex overflow-hidden flex-col items-center border-card full p-4 space-y-2">
+    <AChatImage class="h-16 w-16" :image-url="currentChatPhoto" />
+    <h1 class="text-2xl font-bold">{{ currentChatName }}</h1>
     <div class="w-full flex flex-col overflow-hidden border-card p-4 rounded flex-1">
       <span class="block text-lg font-bold mb-2">Channel Members</span>
       <ul class="overflow-y-auto flex flex-col h-full gap-2">
-        <li v-for="member in chatStore.currentChatMembers">
+        <li v-for="member in currentChatMembers">
           <details class="rounded-md bg-base-200 overflow-hidden">
             <summary class="list-none p-2 flex items-center gap-2 bg-base-200">
               <AChatImage class="h-8 w-8" :image-url="member.avatarUrl" />
