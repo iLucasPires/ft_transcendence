740049dc369c46d5cfa726d8857d3be0ebd0603d
Vinicius Oliveira <vinicius@myriad.codes>
2024-03-10 15:54:17 -0300

    fix: adds size prop to AAvatar

--- apps/frontend/src/components/atoms/AAvatar.vue
+++ apps/frontend/src/components/atoms/AAvatar.vue
@@ -3,13 +3,14 @@ const props = defineProps<{
   username: string;
   avatarUrl?: string;
   isConnected?: boolean;
+  size?: string;
 }>();
 
 const getAvatar = () => props.avatarUrl || `https://robohash.org/${props.username}.png`;
 </script>
 
 <template>
-  <div class="avatar w-24 " :class="isConnected ? 'online' : 'offline'">
+  <div :class="`avatar w-${size ?? '24'} ${isConnected ? 'online' : 'offline'}`">
     <div class="rounded-full bg-base-200">
       <img :src="getAvatar()" :alt="`avatar of ${username}`" />
     </div>
